4d706c54cab36c63bcbc2a4ec2ad976f624ec371
muflikhandimasd <muflikhan.dimas@gmail.com>
2022-06-22 15:37:58 +0700

    Finish

--- app/Http/Controllers/API/WisataController.php
+++ app/Http/Controllers/API/WisataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Helpers\ApiFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Wisata;
+use Exception;
 use Illuminate\Http\Request;
 
 class WisataController extends Controller
@@ -42,7 +43,47 @@ class WisataController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'kategori_id' => 'required',
+                'nama_wisata' => 'required',
+                'slug' => 'required',
+                'harga' => 'required',
+                'deskripsi' => 'required',
+                'kota' => 'required',
+                'provinsi' => 'required',
+                'alamat' => 'required',
+                'waktu_buka' => 'required',
+                'latitude' => 'required',
+                'longitude' => 'required',
+                'image' => 'required'
+            ]);
+
+            $wisata = Wisata::create([
+                'kategori_id' => $request->kategori_id,
+                'nama_wisata' => $request->nama_wisata,
+                'slug' => $request->slug,
+                'harga' => $request->harga,
+                'deskripsi' => $request->deskripsi,
+                'kota' => $request->kota,
+                'provinsi' => $request->provinsi,
+                'alamat' => $request->alamat,
+                'waktu_buka' => $request->waktu_buka,
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+                'image' => $request->image
+            ]);
+
+            $data = Wisata::where('id', '=',  $wisata->id)->get();
+
+            if ($data) {
+                return ApiFormatter::createApi(200, 'Success created data wisata', $data);
+            } else {
+                return ApiFormatter::createApi(400, 'Failed create data wisata', []);
+            }
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(400, 'Failed create data wisata' . $error, []);
+        }
     }
 
     /**
@@ -53,7 +94,13 @@ class WisataController extends Controller
      */
     public function show($id)
     {
-        //
+        $data = Wisata::where('id', '=',  $id)->get();
+
+        if ($data) {
+            return ApiFormatter::createApi(200, 'Success Get Detail data wisata', $data);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed get detail data wisata', []);
+        }
     }
 
     /**
@@ -76,7 +123,34 @@ class WisataController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $wisata = Wisata::findOrFail($id);
+
+            $wisata->update([
+                'kategori_id' => $request->kategori_id,
+                'nama_wisata' => $request->nama_wisata,
+                'slug' => $request->slug,
+                'harga' => $request->harga,
+                'deskripsi' => $request->deskripsi,
+                'kota' => $request->kota,
+                'provinsi' => $request->provinsi,
+                'alamat' => $request->alamat,
+                'waktu_buka' => $request->waktu_buka,
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+                'image' => $request->image
+            ]);
+
+            $data = Wisata::where('id', '=',  $wisata->id)->get();
+
+            if ($data) {
+                return ApiFormatter::createApi(200, 'Success Update data wisata', $data);
+            } else {
+                return ApiFormatter::createApi(400, 'Failed update data wisata', []);
+            }
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(400, 'Failed update data wisata' . $error, []);
+        }
     }
 
     /**
@@ -87,6 +161,12 @@ class WisataController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $wisata = Wisata::findOrFail($id);
+        $data = $wisata->delete();
+        if ($data) {
+            return ApiFormatter::createApi(200, 'Success Delete data wisata', $data);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed Delete data wisata', []);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::get('wisata', [WisataController::class, 'index']);
+Route::get('show-wisata/{id}', [WisataController::class, 'show']);
+Route::post('create-wisata', [WisataController::class, 'store']);
+Route::put('update-wisata/{id}', [WisataController::class, 'update']);
+Route::delete('delete-wisata/{id}', [WisataController::class, 'destroy']);
